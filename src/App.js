33dc7a1b3f4d170cb9ec355ba2630e25d24357db
MikitaAleksandrovich <aleksandrovich.mikita@gmail.com>
2019-06-18 22:47:34 +0400

    Start implementing movie-database project

--- src/App.js
+++ src/App.js
@@ -1,53 +1,45 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import { isExportNamedDeclaration, isImportNamespaceSpecifier } from '@babel/types';
+import Movie from './Movie';
+
+
+const movies = [{
+    id: 1,
+    title: 'Star Wars'
+  }, {
+    id: 2,
+    title: 'Spider Man'
+  }, {
+    id: 3,
+    title: 'Avengers'
+  },
+  {
+    id: 4,
+    title: 'Avengers 2'
+  },
+];
+
 
 class App extends Component {
-
-  state = {
-    input: ''
-  }
-
-  submit = () => {
-    console.log(this.text.value);
-  }
-
-  inputedValue = (event) => {
-    this.setState({
-      input: event.target.value
-    })
-  }
-
-
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-        <Welcome text="Welcome to Using Props" />
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-        <h3>{this.state.input}</h3>
-        <input type="text" onChange={this.inputedValue} value={this.state.input}></input>
-        <input type="text" ref={(input) => this.text = input}></input>
-        <button onClick={this.submit}>Show Value</button>
+
+        {movies.map(movie => <Movie key={movie.id} movie={movie} />)}
+
+
+
+
+
+
+        {movies.map(movie => <Movie key={movie.id} movie={movie}/>)}
       </div>
     );
   }
-}
-
-class Welcome extends Component {
-  render() {
-    const { text } = this.props;
-    return (
-      <div>
-      <h1 className="App-title">{text}</h1>
-      </div>
-    );
-  } 
 }
 
 
