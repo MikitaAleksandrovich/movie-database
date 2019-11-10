e2f5e8dfaf52c52373b168bc07322defeea93035
MikitaAleksandrovich <aleksandrovich.mikita@gmail.com>
2019-11-10 15:08:51 +0300

    Updates

---
+++ precache-manifest.6174a0b20ecf370fc62263e6d40e1e51.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6bcd1f124dba27802541c0947875caec",
+    "url": "/movie-database-React-App/index.html"
+  },
+  {
+    "revision": "f3daa92e0d94750fbc91",
+    "url": "/movie-database-React-App/static/css/main.d1bb2a05.chunk.css"
+  },
+  {
+    "revision": "4c276fc9c3d1db7eb59e",
+    "url": "/movie-database-React-App/static/js/2.dbd380ce.chunk.js"
+  },
+  {
+    "revision": "f3daa92e0d94750fbc91",
+    "url": "/movie-database-React-App/static/js/main.96fed5de.chunk.js"
+  },
+  {
+    "revision": "7ddeb5b52e8ea08cf3b4",
+    "url": "/movie-database-React-App/static/js/runtime~main.24162a3b.js"
+  },
+  {
+    "revision": "ff9a163feb64cd25f5eb42dfece411a4",
+    "url": "/movie-database-React-App/static/media/logo.ff9a163f.svg"
+  }
+]);
